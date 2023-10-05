cf90603e010b721b27c4e331ecac341f23f5730d
ahmaddayekh <adayekh45@gmail.com>
2023-10-06 00:43:08 +0300

    updating our directory page

---
+++ src/Components/Category-item/CategoryItem.js
@@ -0,0 +1,23 @@
+import './Category.scss'
+
+
+const CategoryItem = ({ category }) => {
+    const { imageUrl, title } = category
+    return (
+        <div>
+            <div className="category-container">
+                <div
+                    className="background-image"
+                    style={{
+                        backgroundImage: `Url(${imageUrl})`
+                    }} />
+                <div className="category-body-container">
+                    <h2>{title}</h2>
+                    <p>Shop Now!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
